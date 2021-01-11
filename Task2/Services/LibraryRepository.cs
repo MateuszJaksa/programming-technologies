402c7dd1875d6e2e2907a5cb4f4d134be4b323d7
224083 Baranowski Bartomiej <224083@edu.p.lodz.pl>
2021-01-11 23:48:36 +0100

    Adding using blocks in LibraryRepository
    Signed-off-by: 224083 Baranowski Bartomiej <224083@edu.p.lodz.pl>

--- Task2/Services/LibraryRepository.cs
+++ Task2/Services/LibraryRepository.cs
@@ -7,250 +7,336 @@ namespace Services
 {
     public class LibraryRepository
     {
-        private readonly LinqToSqlDataContext dataContext = new LinqToSqlDataContext();
 
         public LibraryRepository()
         {
-            
+
         }
 
         public int AddCatalog(string title, string author)
         {
-            Catalogs newCatalog = new Catalogs
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
             {
-                Title = title,
-                Author = author
-            };
-            dataContext.Catalogs.InsertOnSubmit(newCatalog);
-            dataContext.SubmitChanges();
-            return newCatalog.ID;
+                Catalogs newCatalog = new Catalogs
+                {
+                    Title = title,
+                    Author = author
+                };
+                dataContext.Catalogs.InsertOnSubmit(newCatalog);
+                dataContext.SubmitChanges();
+                return newCatalog.ID;
+            }
         }
 
         public void UpdateCatalog(int id, string title, string author)
         {
-            Catalogs updatedCatalog = dataContext.Catalogs.FirstOrDefault(catalog => catalog.ID == id);
-            updatedCatalog.Title = title;
-            updatedCatalog.Author = author;
-            dataContext.SubmitChanges();
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                Catalogs updatedCatalog = dataContext.Catalogs.FirstOrDefault(catalog => catalog.ID == id);
+                updatedCatalog.Title = title;
+                updatedCatalog.Author = author;
+                dataContext.SubmitChanges();
+            }
         }
 
         public void RemoveCatalog(int id)
         {
-            bool canBeRemoved = true;
-            foreach (States states in dataContext.States.ToList())
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
             {
-                if (states.CatalogId == id) { canBeRemoved = false; }
-            }
-            if (canBeRemoved)
-            {
-                Catalogs removedCatalog = dataContext.Catalogs.FirstOrDefault(catalog => catalog.ID == id);
-                dataContext.Catalogs.DeleteOnSubmit(removedCatalog);
-                dataContext.SubmitChanges();
+                bool canBeRemoved = true;
+                foreach (States states in dataContext.States.ToList())
+                {
+                    if (states.CatalogId == id) { canBeRemoved = false; }
+                }
+                if (canBeRemoved)
+                {
+                    Catalogs removedCatalog = dataContext.Catalogs.FirstOrDefault(catalog => catalog.ID == id);
+                    dataContext.Catalogs.DeleteOnSubmit(removedCatalog);
+                    dataContext.SubmitChanges();
+                }
             }
         }
 
         public void RemoveAllCatalogs()
         {
-            dataContext.ExecuteCommand(@"DELETE FROM Catalogs;");
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                dataContext.ExecuteCommand(@"DELETE FROM Catalogs;");
+            }
         }
 
         public string GetCatalogTitle(int id)
         {
-            return dataContext.Catalogs.FirstOrDefault(catalog => catalog.ID == id).Title;
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                return dataContext.Catalogs.FirstOrDefault(catalog => catalog.ID == id).Title;
+            }
         }
 
         public string GetCatalogAuthor(int id)
         {
-            return dataContext.Catalogs.FirstOrDefault(catalog => catalog.ID == id).Author;
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                return dataContext.Catalogs.FirstOrDefault(catalog => catalog.ID == id).Author;
+            }
         }
 
         public List<int> GetAllCatalogIds()
         {
-            List<int> returnedList = new List<int>();
-            foreach (Catalogs catalogs in dataContext.Catalogs.ToList())
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
             {
-                returnedList.Add(catalogs.ID);
+                List<int> returnedList = new List<int>();
+                foreach (Catalogs catalogs in dataContext.Catalogs.ToList())
+                {
+                    returnedList.Add(catalogs.ID);
+                }
+                return returnedList;
             }
-            return returnedList;
         }
 
         public int AddState(int catalogId, bool isBorrowed)
         {
-            States newState = new States
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
             {
-                IsBorrowed = isBorrowed,
-                CatalogId = catalogId
-            };
-            dataContext.States.InsertOnSubmit(newState);
-            dataContext.SubmitChanges();
-            return newState.ID;
+                States newState = new States
+                {
+                    IsBorrowed = isBorrowed,
+                    CatalogId = catalogId
+                };
+                dataContext.States.InsertOnSubmit(newState);
+                dataContext.SubmitChanges();
+                return newState.ID;
+            }
         }
 
         public void UpdateState(int id, int catalogId, bool isBorrowed)
         {
-            States updatedState = dataContext.States.FirstOrDefault(state => state.ID == id);
-            updatedState.CatalogId = catalogId;
-            updatedState.IsBorrowed = isBorrowed;
-            dataContext.SubmitChanges();
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                States updatedState = dataContext.States.FirstOrDefault(state => state.ID == id);
+                updatedState.CatalogId = catalogId;
+                updatedState.IsBorrowed = isBorrowed;
+                dataContext.SubmitChanges();
+            }
         }
 
         public void RemoveState(int id)
         {
-            bool canBeRemoved = true;
-            foreach (LibraryEvents events in dataContext.LibraryEvents.ToList())
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
             {
-                if (events.StateId == id) { canBeRemoved = false; }
-            }
-            if (canBeRemoved)
-            {
-                States removedState = dataContext.States.FirstOrDefault(state => state.ID == id);
-                dataContext.States.DeleteOnSubmit(removedState);
-                dataContext.SubmitChanges();
+                bool canBeRemoved = true;
+                foreach (LibraryEvents events in dataContext.LibraryEvents.ToList())
+                {
+                    if (events.StateId == id) { canBeRemoved = false; }
+                }
+                if (canBeRemoved)
+                {
+                    States removedState = dataContext.States.FirstOrDefault(state => state.ID == id);
+                    dataContext.States.DeleteOnSubmit(removedState);
+                    dataContext.SubmitChanges();
+                }
             }
         }
 
         public void RemoveAllStates()
         {
-            dataContext.ExecuteCommand(@"DELETE FROM States;");
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                dataContext.ExecuteCommand(@"DELETE FROM States;");
+            }
         }
 
         public bool GetStateIsBorrowed(int id)
         {
-            return (bool)dataContext.States.FirstOrDefault(state => state.ID == id).IsBorrowed;
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                return (bool)dataContext.States.FirstOrDefault(state => state.ID == id).IsBorrowed;
+            }
         }
 
         public int GetStateCatalogId(int id)
         {
-            return (int)dataContext.States.FirstOrDefault(state => state.ID == id).CatalogId;
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                return (int)dataContext.States.FirstOrDefault(state => state.ID == id).CatalogId;
+            }
         }
 
         public List<int> GetAllStateIds()
         {
-            List<int> returnedList = new List<int>();
-            foreach (States state in dataContext.States.ToList())
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
             {
-                returnedList.Add(state.ID);
+                List<int> returnedList = new List<int>();
+                foreach (States state in dataContext.States.ToList())
+                {
+                    returnedList.Add(state.ID);
+                }
+                return returnedList;
             }
-            return returnedList;
         }
 
         public int AddEvent(DateTime time, int stateId, int userId, bool isBorrowingEvent)
         {
-            LibraryEvents newEvent = new LibraryEvents
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
             {
-                Time = time,
-                StateId = stateId,
-                UserId = userId,
-                isBorrowingEvent = isBorrowingEvent
-            };
-            dataContext.LibraryEvents.InsertOnSubmit(newEvent);
-            dataContext.SubmitChanges();
-            return newEvent.ID;
+                LibraryEvents newEvent = new LibraryEvents
+                {
+                    Time = time,
+                    StateId = stateId,
+                    UserId = userId,
+                    isBorrowingEvent = isBorrowingEvent
+                };
+                dataContext.LibraryEvents.InsertOnSubmit(newEvent);
+                dataContext.SubmitChanges();
+                return newEvent.ID;
+            }
         }
 
         public void UpdateEvent(int id, DateTime time, int stateId, int userId, bool isBorrowingEvent)
         {
-            LibraryEvents updatedEvent = dataContext.LibraryEvents.FirstOrDefault(ev => ev.ID == id);
-            updatedEvent.Time = time;
-            updatedEvent.StateId = stateId;
-            updatedEvent.UserId = userId;
-            updatedEvent.isBorrowingEvent = isBorrowingEvent;
-            dataContext.SubmitChanges();
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                LibraryEvents updatedEvent = dataContext.LibraryEvents.FirstOrDefault(ev => ev.ID == id);
+                updatedEvent.Time = time;
+                updatedEvent.StateId = stateId;
+                updatedEvent.UserId = userId;
+                updatedEvent.isBorrowingEvent = isBorrowingEvent;
+                dataContext.SubmitChanges();
+            }
         }
 
         public void RemoveEvent(int id)
         {
-            LibraryEvents removedEvent = dataContext.LibraryEvents.FirstOrDefault(ev => ev.ID == id);
-            dataContext.LibraryEvents.DeleteOnSubmit(removedEvent);
-            dataContext.SubmitChanges();
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                LibraryEvents removedEvent = dataContext.LibraryEvents.FirstOrDefault(ev => ev.ID == id);
+                dataContext.LibraryEvents.DeleteOnSubmit(removedEvent);
+                dataContext.SubmitChanges();
+            }
         }
 
         public void RemoveAllEvents()
         {
-            dataContext.ExecuteCommand(@"DELETE FROM LibraryEvents;");
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                dataContext.ExecuteCommand(@"DELETE FROM LibraryEvents;");
+            }
         }
 
         public DateTime GetEventTime(int id)
         {
-            return (DateTime)dataContext.LibraryEvents.FirstOrDefault(ev => ev.ID == id).Time;
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                return (DateTime)dataContext.LibraryEvents.FirstOrDefault(ev => ev.ID == id).Time;
+            }
         }
 
         public int GetEventStateId(int id)
         {
-            return (int)dataContext.LibraryEvents.FirstOrDefault(ev => ev.ID == id).StateId;
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                return (int)dataContext.LibraryEvents.FirstOrDefault(ev => ev.ID == id).StateId;
+            }
         }
 
         public int GetEventUserId(int id)
         {
-            return (int)dataContext.LibraryEvents.FirstOrDefault(ev => ev.ID == id).UserId;
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                return (int)dataContext.LibraryEvents.FirstOrDefault(ev => ev.ID == id).UserId;
+            }
         }
 
         public bool GetEventIsBorrowingEvent(int id)
         {
-            return (bool)dataContext.LibraryEvents.FirstOrDefault(ev => ev.ID == id).isBorrowingEvent;
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                return (bool)dataContext.LibraryEvents.FirstOrDefault(ev => ev.ID == id).isBorrowingEvent;
+            }
         }
 
         public List<int> GetAllEventIds()
         {
-            List<int> returnedList = new List<int>();
-            foreach (LibraryEvents ev in dataContext.LibraryEvents.ToList())
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
             {
-                returnedList.Add(ev.ID);
+                List<int> returnedList = new List<int>();
+                foreach (LibraryEvents ev in dataContext.LibraryEvents.ToList())
+                {
+                    returnedList.Add(ev.ID);
+                }
+                return returnedList;
             }
-            return returnedList;
         }
 
         public int AddUser(string name)
         {
-            Users newUser = new Users
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
             {
-                Name = name
-            };
-            dataContext.Users.InsertOnSubmit(newUser);
-            dataContext.SubmitChanges();
-            return newUser.ID;
+                Users newUser = new Users
+                {
+                    Name = name
+                };
+                dataContext.Users.InsertOnSubmit(newUser);
+                dataContext.SubmitChanges();
+                return newUser.ID;
+            }
         }
 
         public void UpdateUser(int id, string name)
         {
-            Users updatedUser = dataContext.Users.FirstOrDefault(user => user.ID == id);
-            updatedUser.Name = name;
-            dataContext.SubmitChanges();
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                Users updatedUser = dataContext.Users.FirstOrDefault(user => user.ID == id);
+                updatedUser.Name = name;
+                dataContext.SubmitChanges();
+            }
         }
 
         public void RemoveUser(int id)
         {
-            bool canBeRemoved = true;
-            foreach (LibraryEvents events in dataContext.LibraryEvents.ToList())
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
             {
-                if (events.UserId == id) { canBeRemoved = false; }
-            }
-            if (canBeRemoved)
-            {
-                Users removedUser = dataContext.Users.FirstOrDefault(user => user.ID == id);
-                dataContext.Users.DeleteOnSubmit(removedUser);
-                dataContext.SubmitChanges();
+                bool canBeRemoved = true;
+                foreach (LibraryEvents events in dataContext.LibraryEvents.ToList())
+                {
+                    if (events.UserId == id) { canBeRemoved = false; }
+                }
+                if (canBeRemoved)
+                {
+                    Users removedUser = dataContext.Users.FirstOrDefault(user => user.ID == id);
+                    dataContext.Users.DeleteOnSubmit(removedUser);
+                    dataContext.SubmitChanges();
+                }
             }
         }
 
         public void RemoveAllUsers()
         {
-            dataContext.ExecuteCommand(@"DELETE FROM Users;");
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                dataContext.ExecuteCommand(@"DELETE FROM Users;");
+            }
         }
 
         public string GetUserName(int id)
         {
-            return dataContext.Users.FirstOrDefault(user => user.ID == id).Name;
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
+            {
+                return dataContext.Users.FirstOrDefault(user => user.ID == id).Name;
+            }
         }
 
         public List<int> GetAllUserIds()
         {
-            List<int> returnedList = new List<int>();
-            foreach (Users user in dataContext.Users.ToList())
+            using (LinqToSqlDataContext dataContext = new LinqToSqlDataContext())
             {
-                returnedList.Add(user.ID);
+                List<int> returnedList = new List<int>();
+                foreach (Users user in dataContext.Users.ToList())
+                {
+                    returnedList.Add(user.ID);
+                }
+                return returnedList;
             }
-            return returnedList;
         }
     }
 }
